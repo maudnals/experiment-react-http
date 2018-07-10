fe821b4b597c0c42d63c69c119bd9fae6beeada7
maudnals <maud.nalpas@gmail.com>
2018-07-10 08:30:00 +0200

    feat - added route parameter

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import Post from '../../../components/Post/Post';
 import classes from './Posts.css';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class Posts extends Component {
 
   state = {
     posts: [],
-    error: false
+    error: false,
+    loading: false
   }
 
   componentDidMount() {
+    this.setState({
+      loading: true
+    });
     axios.get('/posts')
       .then((response) => {
         const posts = response.data.slice(0, 10)
@@ -22,12 +27,14 @@ export default class Posts extends Component {
           });
         this.setState({
           posts: posts,
-          error: false
+          error: false,
+          loading: false
         });
       })
       .catch((error) => {
         this.setState({
-          error: true
+          error: true,
+          loading: false
         });
       });
   }
@@ -39,19 +46,27 @@ export default class Posts extends Component {
   }
 
   render() {
+    const loadingIndicator = 
+      this.state.loading?
+        <p>Loading posts...</p>
+        :
+        null;
     const posts =
       this.state.error ?
         <p>Sorry, something went wrong</p>
         :
         this.state.posts.map(p => {
-          return (<Post
-            key={p.id}
-            author={p.author}
-            title={`${p.title.substring(0, 5)}...`}
-            clicked={this.selectPostHandler.bind(this, p.id)} />);
+          return (
+            <Link to={`/${p.id}`} key={p.id}>
+              <Post
+                author={p.author}
+                title={`${p.title.substring(0, 5)}...`}
+                clicked={this.selectPostHandler.bind(this, p.id)} />
+            </Link>);
         });
     return (
       <section className={classes.Posts}>
+        {loadingIndicator}
         {posts}
       </section>);
   }
